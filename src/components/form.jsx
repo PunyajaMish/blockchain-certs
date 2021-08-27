29a65e7ffcd10272dad46f8ddfe785aeb435307a
PunyajaMish <80346354+PunyajaMish@users.noreply.github.com>
2021-08-27 12:54:06 -0400

    Update form.jsx

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -3,12 +3,18 @@ import { Form, Button } from "react-bootstrap";
 import b1 from "../b3.jpg";
 class Forms extends Component {
   canBeSubmitted() {
-    const { fname, lname, course, email } = this.state;
+    const { fname, lname, course, email, id, studentaddress, colname, coladdress, dateissued } = this.state;
+    //make sure everything is not empty
     return (
-      fname.length > 0 &&
-      lname.length > 0 &&
-      course.length > 0 &&
-      email.length > 0
+      fname.length > 0 && //first name
+      lname.length > 0 && //last name
+      course.length > 0 && //course name
+      email.length > 0 && //email
+      id.length > 0 && //cert id
+      studentaddress.length > 0 && //student address
+      colname.length > 0 && //college name
+      coladdress.length > 0 && //college address
+      dateissued.length > 0
     );
   }
   handleChange = event => {
@@ -27,7 +33,12 @@ class Forms extends Component {
     fname: "",
     lname: "",
     course: "",
-    email: ""
+    email: "",
+    id: "",
+    studentaddress:"",
+    colname:"",
+    coladdress:"",
+    dateissued:""
   };
   render() {
     const isEnabled = this.canBeSubmitted();
@@ -35,7 +46,7 @@ class Forms extends Component {
       <div
         className="container-fluid "
         style={{
-          height: "100vh%",
+          height: "100vw",
           //backgroundImage: `url(${b1})`,
           background: `url(${b1}) no-repeat `,
           backgroundSize: "cover"
@@ -86,20 +97,65 @@ class Forms extends Component {
             </Form.Group>
             <Form.Group>
               <Form.Control
-                type="email"
-                name="email"
-                value={this.state.email}
-                onChange={this.handleChange}
-                placeholder="Email@email.com"
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Control
                 type="text"
                 name="course"
                 value={this.state.course}
                 onChange={this.handleChange}
                 placeholder="Course name"
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Control
+                type="email"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+                placeholder="example@gmail.com"
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Control
+                type="number"
+                name="id"
+                value={this.state.id}
+                onChange={this.handleChange}
+                placeholder="123"
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Control
+                type="text"
+                name="studentaddress"
+                value={this.state.studentaddress}
+                onChange={this.handleChange}
+                placeholder="student address : 0X"
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Control
+                type="text"
+                name="colname"
+                value={this.state.colname}
+                onChange={this.handleChange}
+                placeholder="University of"
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Control
+                type="text"
+                name="coladdress"
+                value={this.state.coladdress}
+                onChange={this.handleChange}
+                placeholder="University address : 0X"
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Control
+                type="date"
+                name="dateissued"
+                value={this.state.dateissued}
+                onChange={this.handleChange}
+                placeholder="yyyy/mm/dd"
               />
             </Form.Group>
             <Button
