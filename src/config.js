6e4a34879ef0a799691160a7456400a848fdd673
PunyajaMish <80346354+PunyajaMish@users.noreply.github.com>
2021-08-27 12:57:37 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,161 +1,150 @@
 import web3 from "./web3";
 
-export const address = "0x598a458f6baf4a7bc6c09007efa9902e393fce77";
+export const address = "0x3820AD05f194663AF4602D253a848A00829E62D2";
 
 export const abi = [
-  {
-    constant: false,
-    inputs: [
-      {
-        name: "name",
-        type: "string"
-      },
-      {
-        name: "course",
-        type: "string"
-      },
-      {
-        name: "email",
-        type: "string"
-      }
-    ],
-    name: "addcert",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function"
-  },
-  {
-    constant: false,
-    inputs: [
-      {
-        name: "coladd",
-        type: "address"
-      },
-      {
-        name: "name",
-        type: "string"
-      }
-    ],
-    name: "addCollege",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function"
-  },
-  {
-    inputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "constructor"
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: false,
-        name: "id",
-        type: "uint256"
-      }
-    ],
-    name: "added",
-    type: "event"
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        name: "",
-        type: "uint256"
-      }
-    ],
-    name: "certs",
-    outputs: [
-      {
-        name: "name",
-        type: "string"
-      },
-      {
-        name: "course",
-        type: "string"
-      },
-      {
-        name: "email",
-        type: "string"
-      },
-      {
-        name: "sender",
-        type: "string"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        name: "col",
-        type: "address"
-      }
-    ],
-    name: "checkcoll",
-    outputs: [
-      {
-        name: "",
-        type: "bool"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        name: "id",
-        type: "uint256"
-      }
-    ],
-    name: "getcert",
-    outputs: [
-      {
-        name: "",
-        type: "string"
-      },
-      {
-        name: "",
-        type: "string"
-      },
-      {
-        name: "",
-        type: "string"
-      },
-      {
-        name: "",
-        type: "string"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "getid",
-    outputs: [
-      {
-        name: "id",
-        type: "uint256"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  }
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "fname",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "lname",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "course",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "colname",
+				"type": "string"
+			}
+		],
+		"name": "certadded",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "name",
+				"type": "string"
+			}
+		],
+		"name": "coll_added",
+		"type": "event"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "coladd",
+				"type": "address"
+			},
+			{
+				"internalType": "string",
+				"name": "name",
+				"type": "string"
+			}
+		],
+		"name": "addCollege",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "fname",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "lname",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "course",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "colname",
+				"type": "string"
+			}
+		],
+		"name": "addcert",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "col",
+				"type": "address"
+			}
+		],
+		"name": "checkcoll",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "sender",
+				"type": "address"
+			}
+		],
+		"name": "viewcert",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "ffname",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
 ];
 
 export default new web3.eth.Contract(abi, address);
